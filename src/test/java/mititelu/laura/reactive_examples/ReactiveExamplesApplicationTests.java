2daa7aa054c795c30304f889de9b43c0d609eec2
LMiti3030 <mititelu_laura@yahoo.com>
2024-07-16 15:33:28 +0300

    initial commit reactive-examples

---
+++ src/test/java/mititelu/laura/reactive_examples/ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package mititelu.laura.reactive_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
